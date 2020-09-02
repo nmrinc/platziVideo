589c10b3447f2cec17adbabe52fa61beb1446e24
Gabino Barrera <nmr.inc@icloud.com>
2020-09-02 02:59:17 -0500

    Footer styles added | component created

--- src/containers/Home/index.jsx
+++ src/containers/Home/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from '../../components/Header';
 import Seeker from '../../components/Seeker';
 import Carousel from '../../components/Carousel';
+import Footer from '../../components/Footer';
 
 const Home = () => (
   <>
@@ -10,6 +11,7 @@ const Home = () => (
   <Carousel category='My list' howMany={10} />
   <Carousel category='Trending' howMany={10} />
   <Carousel category='Platzi Originals' howMany={10} />
+  <Footer />
   </>
 );
 
