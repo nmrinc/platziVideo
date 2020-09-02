a028d16230a337e87ecaef91daf220b601f08bb2
Gabino Barrera <nmr.inc@icloud.com>
2020-09-01 21:59:02 -0500

    SCSS core styles added | App - Main - Home Container structure created | Header component created

---
+++ src/containers/Main/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Home from './../Home';
+
+const Main = () => (
+  <>
+  <Home />
+  </>
+);
+
+export default Main;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import HelloWorld from './components/HelloWorld';
+import App from './App';
+import './assets/scss/Platzi.scss';
 
-ReactDOM.render(<HelloWorld />, document.getElementById('App'));
+ReactDOM.render(<App />, document.getElementById('App'));
