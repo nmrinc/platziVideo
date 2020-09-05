a9c6a2f368512d31a3c818135954133d6fda0d96
Gabino Barrera <nmr.inc@icloud.com>
2020-09-04 21:22:38 -0500

    Switch validation added to router

--- src/routes/App.js
+++ src/routes/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
-import Home from  './../containers/Home';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Home from './../containers/Home';
 import Login from './../containers/Login';
 import Signup from './../containers/Signup';
 
 
 const App = () => (
   <BrowserRouter>
-    <Route exact path='/' component={Home} />
-    <Route exact path='/login' component={Login} />
-    <Route exact path='/signup' component={Signup} />
+    <Switch>
+      <Route exact path='/' component={Home} />
+      <Route exact path='/login' component={Login} />
+      <Route exact path='/signup' component={Signup} />
+    </Switch>
   </BrowserRouter>
 );
 
