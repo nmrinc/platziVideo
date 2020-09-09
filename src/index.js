2733804affd88a4ae3286a32051f9aa9701cc8ea
Gabino Barrera <nmr.inc@icloud.com>
2020-09-08 21:52:18 -0500

    Redux & react-redux installed

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import App from './routes/App';
 import './assets/scss/Platzi.scss';
 
-ReactDOM.render(<App />, document.getElementById('App'));
+ReactDOM.render(
+  <Provider>
+    <App />
+  </Provider>,
+  document.getElementById('App')
+);
