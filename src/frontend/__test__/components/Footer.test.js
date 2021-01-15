2622282baf6ada9577cb9fd537ea112cbac5082f
Gabino Barrera <nmr.inc@icloud.com>
2021-01-14 21:09:13 -0600

    Header tested

--- src/frontend/__test__/components/Footer.test.js
+++ src/frontend/__test__/components/Footer.test.js
@@ -4,31 +4,25 @@
  * If you want create a test for a component, you have to recreate the same structure of the component.
 */
 
-//@a As the component uses React and Redux, you have to recreate the structure so import what's necessary.
+//@a As the component uses React, you have to recreate the structure so import what's necessary.
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
 //@a Import Mount from Enzyme so it help mount the component for test.
 import { mount } from 'enzyme';
 //@a Import "create" to generate a snapshot of the component.
 import { create } from 'react-test-renderer';
 //@a Import the component to be tested.
 import Footer from '../../components/Footer';
-//@a The component is connected to a store. So import it and create its constant.
-import configureStore from '../../configs/configureStore';
-
-const store = configureStore();
+//@a Import the Mock Provider
+import ProviderMock from '../../__mocks__/ProviderMock';
 
 //@o If you will make a series of test, you need to create a suite.
 //@a  With the describe command. Pass the description and the tests.
 describe('<Footer />', () => {
   //@a Create a const with the mounted component wrapped into the redux pattern
   const footer = mount(
-    <Provider store={store}>
-      <BrowserRouter>
-        <Footer />
-      </BrowserRouter>
-    </Provider>
+    <ProviderMock>
+      <Footer />
+    </ProviderMock>
   );
 
   //@a Test if component exists
@@ -51,11 +45,9 @@ describe('<Footer />', () => {
   test('Footer Snapshot', () => {
     //@o This way if there's no snapshot created, will create it. And if there's one, will compare it.
     const footer = create(
-      <Provider store={store}>
-        <BrowserRouter>
-          <Footer />
-        </BrowserRouter>
-      </Provider>
+      <ProviderMock>
+        <Footer />
+      </ProviderMock>
     );
 
     expect(footer.toJSON()).toMatchSnapshot();
