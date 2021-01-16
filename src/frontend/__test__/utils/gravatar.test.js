01f3854a357b54a67232ee520d7c9afea0ccbbde
Gabino Barrera <nmr.inc@icloud.com>
2021-01-15 22:16:30 -0600

    Actions tested

--- src/frontend/__test__/utils/gravatar.test.js
+++ src/frontend/__test__/utils/gravatar.test.js
@@ -2,6 +2,9 @@ import gravatar from '../../utils/gravatar';
 
 test('Gravatar Function test', () => {
   const email = 'oscar@arepa.dev';
+  //@o This url it's obtained from https://es.gravatar.com/site/check/
   const gravatarUrl = 'https://gravatar.com/avatar/4b84ab1122c7ee522710c5ead5525839';
+
+  //@a Test if the url received from the funtion it's equal to the gravatar url.
   expect(gravatarUrl).toEqual(gravatar(email));
 });
