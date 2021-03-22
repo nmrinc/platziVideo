fa9ad30ce49d06ef9ee88635d5735f827cabd318
Erick Avila <nmrinc@CoronaRadiata.local>
2021-03-22 10:58:12 -0600

    Test added to header

--- src/frontend/__test__/components/Header.test.js
+++ src/frontend/__test__/components/Header.test.js
@@ -16,6 +16,44 @@ describe('<Header />', () => {
     expect(header.find('.header__img')).toHaveLength(1);
   });
 
+  test('Logged in User menu', () => {
+    const user = { name: 'Test User', email: 'user@test.com' }
+    const header = mount(
+      <ProviderMock>
+        <Header hasUser={true} user={user} />
+      </ProviderMock>
+    );
+
+    expect(header.find('.gravatar-icon')).toHaveLength(1);
+  });
+
+  test('Not Logged User menu', () => {
+    const header = mount(
+      <ProviderMock>
+        <Header hasUser={false} />
+      </ProviderMock>
+    );
+
+    expect(header.find('.profile-icon')).toHaveLength(1);
+  });
+
+  test('Logout action', () => {
+    const user = { name: 'Test User', email: 'user@test.com' }
+    const logOutAction = jest.fn();
+    const header = mount(
+      <ProviderMock>
+        <Header user={user} hasUser={true} logOutAction={logOutAction} />
+      </ProviderMock>
+    );
+
+    const link = header.find('a[href="#logout"]');
+    link.simulate('click');
+
+    expect(logOutAction).toHaveBeenCalledTimes(1);
+
+    header.unmount();
+  });
+
   test('Header Snapshot', () => {
     const header = create(
       <ProviderMock>
